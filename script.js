3c0cf29b715a311ffee64de39bd904080b12f258
Eric Williams <egw.egwilliams@gmail.com>
2022-07-20 12:53:28 -0400

    Added clear and delete button, no styling

---
+++ script.js
@@ -0,0 +1,4 @@
+// document variables
+const screen = document.getElementById("screen");
+const deleteBtn = document.getElementById("deleteBtn");
+const clearBtn = document.getElementById("clearBtn");
