14d2dd4e30795be04efc0fa3c1c1df2d326815a8
Eric Williams <egw.egwilliams@gmail.com>
2022-07-25 12:42:10 -0400

    Added basic mathematic functions

--- script.js
+++ script.js
@@ -2,3 +2,34 @@
 const screen = document.getElementById("screen");
 const deleteBtn = document.getElementById("deleteBtn");
 const clearBtn = document.getElementById("clearBtn");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// mathematical functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
